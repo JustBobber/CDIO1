0ae05f469af6102e300b7ed11ae3c5b3ed00c7c9
JustBobber <164076237+JustBobber@users.noreply.github.com>
2024-10-04 18:02:55 +0200

    feat: frontend updated

--- game/util/Util.java
+++ game/util/Util.java
@@ -7,6 +7,7 @@ public class Util {
     }
 
     public static void welcome(){
+        System.out.println("");        
         System.out.println(" /$$      /$$           /$$                                             ");
         System.out.println("| $$  /$ | $$          | $$                                             ");
         System.out.println("| $$ /$$$| $$  /$$$$$$ | $$  /$$$$$$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$  ");
