03ae845f38d67ec766239388d9abeabeca0cab8a
LukasOegaard <s244942@dtu.dk>
2024-10-04 14:51:21 +0200

    refac: improved readability in main game loop

--- game/Game.java
+++ game/Game.java
@@ -1,7 +1,7 @@
 package game;
 import game.util.Util;
 import java.util.Scanner;
-// TODO: singleton
+
 public class Game {
 
     private Player player1;
@@ -18,19 +18,26 @@ public class Game {
         this.die = new Die();
     }
 
+    /**
+     * wait for a user to press enter
+     */
+    private void waitForEnter() {
+        this.scanner.nextLine(); 
+    }
+
     public void start() {
         Util.print("game started");
         while (true) { 
             if (round % 2 == 0) {
                 Util.print("player 1 press enter to roll");
-                this.scanner.nextLine(); // wait for player to roll
+                waitForEnter();
                 int roll1 = this.die.roll().getFaceValue();
                 int roll2 = this.die.roll().getFaceValue();
                 Util.print("player 1 rolled " + (roll1+roll2));
                 this.player1.addScore(roll1+roll2);
             } else {
                 Util.print("player 2 press enter to roll");
-                this.scanner.nextLine(); // wait for player to roll
+                waitForEnter();
                 int roll1 = this.die.roll().getFaceValue();
                 int roll2 = this.die.roll().getFaceValue();
                 Util.print("player 2 rolled " + (roll1+roll2));
